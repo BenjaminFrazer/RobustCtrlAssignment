fcf0ab91ae82899ae00e9b63916695f15b340af1
benjaminf <benjamin.frazer.1997@gmail.com>
2022-03-24 09:45:52 +0000

    progress

---
+++ Section2/Q2_NeuralController/NeuralController_Homing.m
@@ -0,0 +1,19 @@
+% Neural Network 
+function [out] = NeuralController_Homing(input,Params)
+%     Params.T1 = 0;
+%     Params.T2 = 0.2;
+%     Params.W1 =+1;
+%     Params.W2 =-1;
+%     Params.W3 =-1.219;%M2
+%     Params.W4 =0.993;%M1
+% 
+%     input.LS =  1;
+%     input.RS = 1;
+%% calculate output
+ML = (input.LS*Params.W1+input.RS*Params.W3)>Params.T1; %1
+MR = (input.RS*Params.W4+input.LS*Params.W2)>Params.T2; %2
+
+%%
+out.ML = ML;
+out.MR = MR;
+end
